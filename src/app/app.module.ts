8e9792e68d1cefcf4b2ab8b11a9523450335b0c9
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2021-03-11 18:49:23 +0100

    resolution bug contact page, ajout connection service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactPageComponent } from './contact-page/contact-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ContactPageComponent} from './contact-page/contact-page.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {ConnectionService} from "./contact-page/connection.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -22,9 +25,11 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
     BrowserModule,
     MDBBootstrapModule.forRoot(),
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [ConnectionService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ConnectionService } from './connection.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {ConnectionService} from './connection.service';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -10,8 +10,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class ContactPageComponent implements OnInit {
 
   contactForm: FormGroup;
-disabledSubmitButton: boolean = true;
-optionsSelect: Array<any>;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any>;
 
   @HostListener('input') oninput() {
 
@@ -21,38 +21,41 @@ optionsSelect: Array<any>;
   }
 
   constructor(fb: FormBuilder, private connectionService: ConnectionService) {
-
-  this.contactForm = fb.group({
-    'contactFormName': ['', Validators.required],
-    'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
-    'contactFormSubjects': ['', Validators.required],
-    'contactFormMessage': ['', Validators.required],
-    'contactFormCopy': ['', Validators.requiredTrue],
+    this.contactForm = fb.group({
+      'contactFormName': ['', Validators.required],
+      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+      'contactFormSubjects': ['', Validators.required],
+      'contactFormMessage': ['', Validators.required],
+      'contactFormCopy': ['', Validators.requiredTrue],
     });
   }
 
   ngOnInit() {
 
-  this.optionsSelect = [
-    { value: 'Feedback', label: 'Feedback' },
-    { value: 'Report a bug', label: 'Report a bug' },
-    { value: 'Feature request', label: 'Feature request' },
-    { value: 'Other stuff', label: 'Other stuff' },
+    this.optionsSelect = [
+      {value: 'Feedback', label: 'Feedback'},
+      {value: 'Report a bug', label: 'Report a bug'},
+      {value: 'Feature request', label: 'Feature request'},
+      {value: 'Other stuff', label: 'Other stuff'},
     ];
   }
 
   get name() {
     return this.contactForm.get('contactFormName');
   }
+
   get email() {
     return this.contactForm.get('contactFormEmail');
   }
+
   get subjects() {
     return this.contactForm.get('contactFormSubjects');
   }
+
   get message() {
     return this.contactForm.get('contactFormMessage');
   }
+
   get copy() {
     return this.contactForm.get('contactFormCopy');
   }
