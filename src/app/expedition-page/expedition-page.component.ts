9f05a95fc603b6dc598e5c0c4da3492b0d92bc2f
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2021-03-12 20:11:32 +0100

    Ajout du service des expéditions

--- src/app/expedition-page/expedition-page.component.ts
+++ src/app/expedition-page/expedition-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-expedition-page',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpeditionPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ExpeditionService: ExpeditionService) {
+  }
 
   ngOnInit(): void {
   }
+
 
 }
