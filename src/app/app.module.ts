5cd68a8add6dca0e4e2066170138bf1d8f8ee2eb
Faure-Thomas <56997397+Faure-Thomas@users.noreply.github.com>
2021-03-23 11:49:44 +0100

    added button component in shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {SharedModule} from './shared/shared.module';
+import {ButtonModule} from './shared/button/button.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
