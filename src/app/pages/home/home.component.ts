7db50baafaad780ff1626e15f42d0d153982ed6a
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2021-04-25 12:29:26 +0200

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from "rxjs";
+import {IActualite} from "../actualite/actualite.interface";
+import {ActualiteService} from "../actualite/actualite.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private subscription: Subscription;
+  actualite: IActualite;
+  constructor(private actualiteService: ActualiteService) {
+  }
 
   ngOnInit(): void {
+    this.subscription = this.actualiteService.actualites$.subscribe(() => {
+      this.actualite = this.actualiteService.getLast();
+      console.log(this.actualite);
+    });
   }
 
 }
