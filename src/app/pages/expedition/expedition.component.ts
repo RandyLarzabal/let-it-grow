27b9d284f2034b3593be06a45cec4bfe17b0c575
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2021-04-25 16:29:02 +0200

    update

--- src/app/pages/expedition/expedition.component.ts
+++ src/app/pages/expedition/expedition.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {ExpeditionService} from './expedition.service';
 
@@ -9,9 +9,23 @@ import {ExpeditionService} from './expedition.service';
 })
 export class ExpeditionComponent implements OnInit {
 
-  constructor(private ExpeditionService: ExpeditionService) { }
 
-  ngOnInit(): void {
+  images: any[] = [{
+    src: '../assets/img/expedition/vietnam.png'
+  }, {src: '../assets/img/expedition/inde.png'}, {src: '../assets/img/expedition/amazonie.png'},]
+
+
+  constructor(private ExpeditionService: ExpeditionService
+  ) {
+  }
+
+  ngOnInit()
+    :
+    void {
+  }
+
+  handleImageClick(src) {
+    console.log(src);
   }
 
 }
